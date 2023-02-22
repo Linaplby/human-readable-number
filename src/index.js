90e0ff2cfda97c3aab7c9703ef083b0920141fd5
Linaplby <119460484+Linaplby@users.noreply.github.com>
2023-02-22 14:29:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,25 +33,4 @@ module.exports = function toReadable (number) {
     }
     console.log(res)
     
-    function parseHundreds(num) {
-        n = ['one hundred ','two hundred ','three hundred ','four hundred ','five hundred ',
-    'six hundred ','seven hundred ','eight hundred ','nine hundred ','nine hundred ']
-    console.log(n[parseInt(x.slice(x.length-3,x.length-2))-1])
-        return (n[parseInt(x.slice(x.length-3,x.length-2))]!=undefined)?
-    n[parseInt(x.slice(x.length-3,x.length-2))-1]:'';
-    
-    }
-    
-    function parseDecades(num) {
-        n = ['ten ','twenty ','thirty ','forty ','fifty ','sixty ','seventy ','eighty ','ninety ']
-        return  n[parseInt(x.slice(x.length-2,x.length-1))-1]
-    }
-    
-    function parseNumerals(num) {
-        n = ['zero','one','two','three','four','five','six','seven','eight','nine']
-        return n[parseInt(x.slice(x.length-1,x.length))]
-    }
-    
-    return res.split('undefined').join("")
-} 
-
+   
